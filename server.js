2a1723a8daa23f2f2c20ca1005e235ce8967825e
gugulen0k <terzi.kirill.ceiti@gmail.com>
2022-03-10 21:22:23 +0200

    changed server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import router from './routes/taskRoutes.js';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
